1d1a2684c7a2537c2f1c255efeacbb665ef03019
vimal joshi <111500483+vimaljoshi172003@users.noreply.github.com>
2024-10-13 11:56:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const { Chess } = require("chess.js");
+const path = require("path");
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
+let players = {};
+let currentPlayer = "W";
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index", { title: "Chess Game" })
+})
+io.on("connection", (socket) => {
+    console.log("Connected");
+    if (!players.white) {
+        players.white = socket.id;
+        socket.emit("playerRole", "w")
+    }
+    else if (!players.black) {
+        players.black = socket.id;
+        socket.emit("playerRole", "b")
+    }
+    else {
+        socket.emit("spectatorRole")
+    }
+    socket.on("disconnect", () => {
+        if (socket.id === players.white) {
+            delete players.white;
+        }
+        else if (socket.id === players.black) {
+            delete players.black;
+        }
+    });
+    socket.on("move", (move) => {
+        try {
+            if (chess.turn() === "w" && socket.id !== players.white) return;
+            if (chess.turn() === "b" && socket.id !== players.black) return;
+
+            const result = chess.move(move);
+            if (result) {
+                currentPlayer = chess.turn();
+                io.emit("move", move);
+                io.emit("boardState", chess.fen())
+            }
+            else {
+                console.log("Invalid move :", move);
+                socket.emit("invalidMove", move);
+            }
+        }
+        catch (err) {
+            console.log(err);
+            socket.emit("invalidMove", move);
+        }
+    })
+})
+server.listen(3000)
